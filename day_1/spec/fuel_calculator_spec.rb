08bbab50c2855f485ac68f5034752f2511c9ca35
Brad Phillips <bjrp@protonmail.com>
2020-05-08 22:31:08 +0100

    Day 1 - complete

--- day_1/spec/fuel_calculator_spec.rb
+++ day_1/spec/fuel_calculator_spec.rb
@@ -1,16 +1,23 @@
 require 'fuel_calculator'
 
 describe 'fuel_calculator' do
-  it 'returns 2 when passed 12' do
-    expect(fuel_calculator(12)).to eq(2)
+  context 'one module passed - initial acceptance criteria' do
+    it 'returns 2 when passed 12' do
+      expect(fuel_calculator(12)).to eq(2)
+    end
+    it 'returns 2 when passed 14' do
+      expect(fuel_calculator(14)).to eq(2)
+    end
+    it 'returns 654 when passed 1969' do
+      expect(fuel_calculator(1969)).to eq(654)
+    end
+    it 'returns 33583 when passed 100756' do
+      expect(fuel_calculator(100756)).to eq(33583)
+    end
   end
-  it 'returns 2 when passed 14' do
-    expect(fuel_calculator(14)).to eq(2)
-  end
-  it 'returns 654 when passed 1969' do
-    expect(fuel_calculator(1969)).to eq(654)
-  end
-  it 'returns 33583 when passed 100756' do
-    expect(fuel_calculator(100756)).to eq(33583)
+  context 'multiple module inputs' do
+    it 'returns 4 when passed 12,12' do
+      expect(fuel_calculator(12,12)).to eq(4)
+    end
   end
 end
