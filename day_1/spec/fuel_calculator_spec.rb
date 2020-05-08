e49d2b2ef364da01e706736b267a3ebe19d52b2f
Brad Phillips <bjrp@protonmail.com>
2020-05-08 22:04:04 +0100

    Day 1 - unit test two

--- day_1/spec/fuel_calculator_spec.rb
+++ day_1/spec/fuel_calculator_spec.rb
@@ -1,7 +1,10 @@
 require 'fuel_calculator'
 
 describe 'fuel_calculator' do
-  it 'returns 12 when passed 2' do
+  it 'returns 2 when passed 12' do
     expect(fuel_calculator(12)).to eq(2)
+  end
+  it 'returns 2 when passed 14' do
+    expect(fuel_calculator(14)).to eq(2)
   end
 end
