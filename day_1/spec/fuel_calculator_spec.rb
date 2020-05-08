40ca45b0a79db94ad09d3ae71e2054c9fb110b23
Brad Phillips <bjrp@protonmail.com>
2020-05-08 22:07:27 +0100

    Day 1 - passes all provided criteria

--- day_1/spec/fuel_calculator_spec.rb
+++ day_1/spec/fuel_calculator_spec.rb
@@ -7,4 +7,10 @@ describe 'fuel_calculator' do
   it 'returns 2 when passed 14' do
     expect(fuel_calculator(14)).to eq(2)
   end
+  it 'returns 654 when passed 1969' do
+    expect(fuel_calculator(1969)).to eq(654)
+  end
+  it 'returns 33583 when passed 100756' do
+    expect(fuel_calculator(100756)).to eq(33583)
+  end
 end
