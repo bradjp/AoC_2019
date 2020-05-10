8534859147891eae6780f9fc64b8f6f0ef417354
Brad Phillips <bjrp@protonmail.com>
2020-05-10 23:43:30 +0100

    Day 2 - altered test expectation

--- day_2/spec/intcode_spec.rb
+++ day_2/spec/intcode_spec.rb
@@ -2,6 +2,6 @@ require 'intcode'
 
 describe 'intcode' do
   it 'returns 2,0,0,0,99 when passed [1,0,0,0,99]' do
-    expect(intcode(2,0,0,0,99)).to eq(1,99)
+    expect(intcode(1,0,0,0,99)).to eq('1,0,0,0,99')
   end
 end
